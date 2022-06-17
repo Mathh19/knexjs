be82e91b6c6747ea8cd291c3ee6982ffc76c1a0f
Matheus <mf635097@gmail.com>
2022-06-17 08:12:44 -0300

    Fazendo select com where basico

---
+++ knex/queries/script-select-where-basic.js
@@ -0,0 +1,16 @@
+const knex = require('../config/database');
+
+const select = knex('users as u').select('id', 'first_name')
+    .where('id', '=', 2)
+    .andWhere('first_name', '=', 'Leon')
+    .orWhere('id', '=', 3);
+
+select.then(data => {
+    for (const user of data) {
+        console.log(user);
+    };
+}).catch(e => {
+    console.log('ERRO:', e.message);
+}).finally(() => {
+    knex.destroy();
+});
