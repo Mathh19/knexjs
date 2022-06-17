9e2c66aaeebb8b9622eeb4151919e6c93f57ddc9
Matheus <mf635097@gmail.com>
2022-06-17 08:35:43 -0300

    Inserindo dados ficticios com knex.raw

---
+++ knex/queries/script-insert-datas-dummy.js
@@ -0,0 +1,11 @@
+const knex = require('../config/database');
+
+const query = datas;
+
+knex.raw(query).then(data => {
+    console.log(data);
+}).catch(e => {
+    console.log(e.message);
+}).finally(() => {
+    knex.destroy();
+});
