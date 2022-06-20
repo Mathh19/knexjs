cfe2c4f0f30698277e902dcd37a220b227e96aa2
Matheus <mf635097@gmail.com>
2022-06-20 06:31:25 -0300

    Select Order By com Knex

---
+++ knex/queries/script-select-order.js
@@ -0,0 +1,17 @@
+const knex = require('../config/database');
+const selectOrder = knex('users')
+    .select('id', 'first_name')
+    .where('first_name', 'like', '___')
+    .orWhere('first_name', 'like', '%na')
+    .orderBy('id', 'asc')
+    .orderBy('first_name', 'desc');
+
+console.log(selectOrder.toString());
+
+selectOrder.then((data) => {
+    console.log(data);
+}).catch((e) => {
+    console.log(e.message);
+}).finally(() => {
+    knex.destroy();
+});
