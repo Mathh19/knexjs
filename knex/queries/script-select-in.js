171bb4c230e4e6d51325492f0c18974d996344ee
Matheus <mf635097@gmail.com>
2022-06-20 06:10:59 -0300

    select IN com Knex

---
+++ knex/queries/script-select-in.js
@@ -0,0 +1,14 @@
+const knex = require('../config/database');
+const selectIn = knex('users')
+    .select('id', 'first_name')
+    .whereIn('id', [10, 15, 20, 25]);
+
+console.log(selectIn.toString());
+
+selectIn.then((data) => {
+    console.log(data);
+}).catch((e) => {
+    console.log(e.message);
+}).finally(() => {
+    knex.destroy();
+});
